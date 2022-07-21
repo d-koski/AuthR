82a287eb4e89e4bd56c81c92d576969ef55e28b9
Damian Koski <damian.koski97@gmail.com>
2022-07-21 19:19:48 +0200

    Setup project hierarchy

---
+++ src/AuthR.BusinessLogic/DependencyInjectionConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using AuthR.DataAccess;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthR.BusinessLogic;
+
+public static class DependencyInjectionConfiguration
+{
+    public static void AddBusinessLogic(this IServiceCollection services)
+    {
+        services.AddDataAccess();
+    }
+}
---
+++ src/AuthR.DataAccess/DependencyInjectionConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthR.DataAccess;
+
+public static class DependencyInjectionConfiguration
+{
+    public static void AddDataAccess(this IServiceCollection services)
+    {
+    }
+}
